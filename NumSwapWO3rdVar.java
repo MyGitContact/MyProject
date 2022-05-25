cb855b1be1158713e72c20dd196bc0b3a2603005
ajay <contactme.mymailbox@gmail.com>
2022-05-25 20:08:10 +0530

    Number swap using 3rd var

---
+++ NumSwapWO3rdVar.java
@@ -0,0 +1,22 @@
+public class NumSwapWO3rdVar
+{
+public static void main(String[] args)
+{
+
+
+int a=20;
+int b=30;
+
+System.out.println("Values before Swapping are : ");	
+System.out.println("a = "+a+" "+"b = "+b);
+
+a=a+b;//50
+b=a-b;//20
+a=a-b;
+
+
+System.out.println("Values before Swapping are : ");	
+System.out.println("a = "+a+" "+"b = "+b);
+}
+
+}
